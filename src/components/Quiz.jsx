ca3c9649669e3be83617d3f7ac8ed3526c6fbd9d
k-m-rahman <k_m_rahman23@hotmail.com>
2022-10-12 16:54:22 +0600

    did some styling in marksheet

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -43,25 +43,33 @@ const Quiz = () => {
               <h1 className="text-xl font-semibold mb-2 underline">
                 Mark Sheet
               </h1>
-              <div className="text-lg font-medium flex flex-col gap-2 mt-9  w-11/12 shadow-md border border-gray-300 rounded-lg p-4 mx-auto text-right">
-                <h3 className="grid grid-cols-6">
+              <div className="text-lg font-medium flex flex-col gap-2 my-9  w-11/12 shadow-md border border-gray-300 rounded-lg p-4 mx-auto ">
+                <h3 className="grid grid-cols-6  text-center lg:text-left">
                   {" "}
-                  <span className="col-span-5">Total Questions :</span>{" "}
+                  <span className="col-span-4  lg:col-span-5 ">
+                    Total Questions :
+                  </span>{" "}
                   <span className="ml-3">{total}</span>{" "}
                 </h3>
-                <h3 className="grid grid-cols-6">
+                <h3 className="grid grid-cols-6  text-center lg:text-left">
                   {" "}
-                  <span className="col-span-5">Correct Answers :</span>{" "}
+                  <span className="col-span-4  lg:col-span-5 ">
+                    Correct Answers :
+                  </span>{" "}
                   <span className="ml-3">{correctAns}</span>{" "}
                 </h3>
-                <h3 className="grid grid-cols-6">
+                <h3 className="grid grid-cols-6  text-center lg:text-left">
                   {" "}
-                  <span className="col-span-5">Wrong Answers :</span>{" "}
+                  <span className="col-span-4  lg:col-span-5 ">
+                    Wrong Answers :
+                  </span>{" "}
                   <span className="ml-3">{wrongAns}</span>{" "}
                 </h3>
-                <h3 className="grid grid-cols-6">
+                <h3 className="grid grid-cols-6  text-center lg:text-left">
                   {" "}
-                  <span className="col-span-5">Remaining :</span>{" "}
+                  <span className="col-span-4  lg:col-span-5 ">
+                    Remaining :
+                  </span>{" "}
                   <span className="ml-3">{total - correctAns - wrongAns}</span>{" "}
                 </h3>
               </div>
